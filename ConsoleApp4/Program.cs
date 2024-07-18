d680407d5b780aee89e18111c2ef940719e25ba6
Rohat Güneş <rohatgns44@gmail.com>
2024-07-19 06:42:23 +1000

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan bir sayı al
+        Console.Write("Lütfen bir sayı giriniz: ");
+        int sayi;
+
+        
+        while (!int.TryParse(Console.ReadLine(), out sayi))
+        {
+            Console.Write("Geçerli bir sayı giriniz: ");
+        }
+
+        // Sayının 10'a eşit, küçük veya büyük olup olmadığını kontrol et
+        if (sayi == 10 && sayi % 2 == 0 )
+        {
+            Console.WriteLine("Girdiğiniz sayı 10'a eşittir ve çifttir ");
+        }
+        else if (sayi < 10  )
+        {
+            Console.WriteLine("Girdiğiniz sayı 10'dan küçüktür.");
+        }
+        else
+        {
+            Console.WriteLine("Girdiğiniz sayı 10'dan büyüktür.");
+        }
+
+        // Sayının çift mi tek mi olduğunu kontrol et
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Girdiğiniz sayı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Girdiğiniz sayı tektir.");
+        }
+    }
+}
